ca11af2063dc2a194ac71751d659cfc6e5840e63
ShawnEvans77 <shawnevans328@gmail.com>
2025-02-25 19:00:09 -0500

    ye

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,7 @@ import React from 'react';
 export default function Home() {
   return (
       <div>
-      <h1>Welcome to the Home Page!</h1>
-
+      <h1>brooklyn college cs 1115</h1>
       <hr/>
       </div>  
   );
